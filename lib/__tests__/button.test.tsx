c32c5ae74a742c6ea2522f42075da22755ee9cdb
方欣芮 <sssrrr@vip.qq.com>
2020-05-19 18:12:48 +0800

    feat: 配置 jest

---
+++ lib/__tests__/button.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from '../button';
+
+describe('button', () => {
+  it('是个 div', () => {
+    const json = renderer.create(<Button/>).toJSON();
+    expect(json).toMatchSnapshot();
+  });
+});
