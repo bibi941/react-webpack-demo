1e2335e90cc49c14cfe3605651b653dd5b71f4a9
方欣芮 <sssrrr@vip.qq.com>
2020-05-29 16:36:36 +0800

    fix: 修改package.json的 files

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon';
-
-const App = () => {
-  const fn: React.MouseEventHandler = (e) => {
-    console.log(e);
-  };
-  return (
-    <div>
-      <Icon name='browse' onClick={fn}/>
-      <Icon name='attachent' className={'aaa'}/>
-    </div>
-  );
-};
-
-ReactDom.render(<App/>, document.querySelector('#root'));
+export { default as Icon } from './icon/icon';
